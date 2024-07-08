2ffdce2beb1e1206b8cba2c3b744c8cd69c878d5
ram7909 <ramcharanmain07@gmail.com>
2024-07-08 13:40:24 +0530

    News App

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,7 +4,8 @@ const News = () => {
     const [news, setNews] = useState([])
     const fetchData = async (category) => {
         const apiKey = 'e519bc933e8641c0b7ef243d14da2fb5'
-        const data = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`)
+        const data = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2024-06-08&sortBy=publishedAt&apiKey=e519bc933e8641c0b7ef243d14da2fb5`)
+        // const data = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`)
         const result = await data.json();
         setNews(result.articles)
         console.log(result.articles);
@@ -18,13 +19,13 @@ const News = () => {
     return (
         <>
             <div className="head text-center my-3" >
-                <button type="button" onClick={()=>fetchData('general')} className="btn btn-primary mx-3 my-2">General</button>
-                <button type="button" onClick={()=>fetchData('health')} className="btn btn-primary mx-3 my-2">Health</button>
-                <button type="button" onClick={()=>fetchData('science')} className="btn btn-primary mx-3 my-2">Science</button>
-                <button type="button" onClick={()=>fetchData('technology')} className="btn btn-primary mx-3 my-2">Technology</button>
-                <button type="button" onClick={()=>fetchData('entertainment')} className="btn btn-primary mx-3 my-2">Entertainment</button>
-                <button type="button" onClick={()=>fetchData('sports')} className="btn btn-primary mx-3 my-2">Sports</button>
-                <button type="button" onClick={()=>fetchData('business')} className="btn btn-primary mx-3 my-2">Business</button>
+                <button type="button" onClick={() => fetchData('general')} className="btn btn-primary mx-3 my-2">General</button>
+                <button type="button" onClick={() => fetchData('health')} className="btn btn-primary mx-3 my-2">Health</button>
+                <button type="button" onClick={() => fetchData('science')} className="btn btn-primary mx-3 my-2">Science</button>
+                <button type="button" onClick={() => fetchData('technology')} className="btn btn-primary mx-3 my-2">Technology</button>
+                <button type="button" onClick={() => fetchData('entertainment')} className="btn btn-primary mx-3 my-2">Entertainment</button>
+                <button type="button" onClick={() => fetchData('sports')} className="btn btn-primary mx-3 my-2">Sports</button>
+                <button type="button" onClick={() => fetchData('business')} className="btn btn-primary mx-3 my-2">Business</button>
             </div>
             <div className="main-s mx-5 d-flex justify-content-center">
                 <div className="container my-3 d-flex justify-content-center">
